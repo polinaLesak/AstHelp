e33475be38a562334fc3b421ed9ebd1f27e4362f
polinaLesak <lesakpolina@gmail.com>
2024-10-14 22:16:41 +0300

    add app gateway

---
+++ Gateway.WebApi/Program.cs
@@ -0,0 +1,32 @@
+using Gateway.WebApi.Config;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var routes = "Routes";
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile($"{routes}/ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"{routes}/ocelot.SwaggerEndPoints.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+}
+
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+    opt.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+}).UseOcelot().Wait();
+
+app.Run();
