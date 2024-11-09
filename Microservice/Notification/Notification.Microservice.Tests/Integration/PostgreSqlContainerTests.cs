c177c754a2750342e242f9598e65030fe079d755
polinaLesak <lesakpolina@gmail.com>
2024-11-10 00:24:05 +0300

    add notification container integrational test fixtures

---
+++ Microservice/Notification/Notification.Microservice.Tests/Integration/PostgreSqlContainerTests.cs
@@ -0,0 +1,27 @@
+﻿using Notification.Microservice.Tests.Integration.Configuration;
+
+namespace Notification.Microservice.Tests.Integration
+{
+    public class PostgreSqlContainerTests : IClassFixture<PostgreSqlContainerFixture>
+    {
+        private readonly PostgreSqlContainerFixture _fixture;
+
+        public PostgreSqlContainerTests(PostgreSqlContainerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task TestConnectionToPostgreSql()
+        {
+            var connectionString = _fixture.ConnectionString;
+
+            await using (var connection = new Npgsql.NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                Assert.Equal("TestDb", connection.Database);
+            }
+        }
+    }
+}
