0173259f3edd97ffff20ca469c15ea04bc376d8c
polinaLesak <lesakpolina@gmail.com>
2025-03-08 13:26:50 +0300

    fix prod docker build and add backups of pending database

--- AstHelp.Webapp/public/env-config.js
+++ AstHelp.Webapp/public/env-config.js
@@ -1,5 +1,4 @@
 window.env = {
-  REACT_APP_API_URL: "https://localhost/api/",
-  // REACT_APP_API_URL: "http://localhost:80/api/",
+  REACT_APP_API_URL: "https://localhost/backend/api/",
   REACT_APP_ENVIRONMENT: "development"
 };
--- AstHelp.Webapp/src/config.js
+++ AstHelp.Webapp/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: window.env?.REACT_APP_API_URL || "http://localhost:5000/api/",
+  apiUrl: window.env?.REACT_APP_API_URL || "https://localhost/backend/api/",
 };
 
 export default config;
